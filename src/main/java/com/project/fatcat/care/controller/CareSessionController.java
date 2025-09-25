ca43a0bfe383f6d6b94583b2b4251ecb37ae9b14
duckmanni <miso7365@gmail.com>
2025-09-25 20:20:12 +0900

    mijin_websocket_5
    돌봄 리뷰목록 + 등록

--- src/main/java/com/project/fatcat/care/controller/CareSessionController.java
+++ src/main/java/com/project/fatcat/care/controller/CareSessionController.java
@@ -1,17 +1,24 @@
 package com.project.fatcat.care.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import com.project.fatcat.care.dto.CareSessionDto;
 import com.project.fatcat.care.service.CareSessionService;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/chat/care")
 public class CareSessionController {
 
-	private final CareSessionService careSessionService;
-	
+    private final CareSessionService careSessionService;
 
-	
-	}
+    // 돌봄 확정 API
+    @PutMapping("/confirm/{sessionId}")
+    public ResponseEntity<CareSessionDto> confirmCare(@PathVariable Integer sessionId) {
+        CareSessionDto confirmed = careSessionService.confirmSession(sessionId);
+        return ResponseEntity.ok(confirmed);
+    }
+}
