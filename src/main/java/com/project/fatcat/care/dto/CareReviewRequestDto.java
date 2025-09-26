b87b1eb22aaae2144e7f85487edfedda938462f7
duckmanni <miso7365@gmail.com>
2025-09-26 13:55:55 +0900

    mijin_Websocket_5
    ㅋㅋㅋㅋㅋㅋ

--- src/main/java/com/project/fatcat/care/dto/CareReviewRequestDto.java
+++ src/main/java/com/project/fatcat/care/dto/CareReviewRequestDto.java
@@ -6,6 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CareReviewRequestDto {
+	
+	private Integer authorUserSeq; 
+	
+	
     private Integer targetUserSeq;
     private String careReview;
     private Integer careRating;
