e155f17a83157cc9a215ec735dad8881bdfa93aa
uujinnn <kimuujin6422@gmail.com>
2025-09-28 16:04:36 +0900

    로그인 수정

--- src/main/java/com/project/fatcat/users/service/CustomUserDetailsService.java
+++ src/main/java/com/project/fatcat/users/service/CustomUserDetailsService.java
@@ -1,5 +1,26 @@
 package com.project.fatcat.users.service;
 
-public class CustomUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.project.fatcat.entity.User;
+import com.project.fatcat.users.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	private final UserRepository userRepository;
+	
+	@Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUserEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다: " + userEmail));
+        return new CustomUserDetails(user); // ✅ User → UserDetails 변환
+    }
 
 }
