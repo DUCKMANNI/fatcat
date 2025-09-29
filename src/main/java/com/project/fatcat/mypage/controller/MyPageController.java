61e8e8ce36a7a6159e30eb80020c029d43da4e79
yujeong0-0 <dbwjd4192@gmail.com>
2025-09-29 15:56:40 +0900

    dmdk
    쏘리이

--- src/main/java/com/project/fatcat/mypage/controller/MyPageController.java
+++ src/main/java/com/project/fatcat/mypage/controller/MyPageController.java
@@ -16,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/mypage")
+//@RequestMapping("/mypage")
 public class MyPageController {
     
 
@@ -26,22 +26,21 @@ public class MyPageController {
     // 마이페이지 URL로 접속하면 이 메소드가 호출됩니다.
     @GetMapping
     public String myPage(Model model) {
-        // TODO: 로그인된 사용자의 실제 ID를 가져와야 합니다.
-        // 현재는 테스트를 위해 임시로 userId 1을 사용합니다.
-        int userId = 1; 
+        
+        int userSeq = 1; 
 
-        // 1. CatService를 사용하여 현재 사용자의 고양이 리스트를 가져옵니다.
-        List<Cat> cats = catService.findAllByUserId(userId);
+        
+        List<Cat> cats = catService.findAllByUserId(userSeq);
 
-        // 2. UserRepository를 사용하여 현재 로그인된 사용자의 정보를 가져옵니다.
-        User user = userRepository.findById(userId)
+        
+        User user = userRepository.findById(userSeq)
                                   .orElseThrow(() -> new RuntimeException("User not found"));
 
-        // 3. 가져온 'cats' 리스트와 'user' 객체를 'model'에 담아 HTML 템플릿으로 전달합니다.
+       
         model.addAttribute("cats", cats);
         model.addAttribute("user", user);
         
-        // 4. 'mypage.html' 템플릿을 반환하여 렌더링합니다.
+       
         return "mypage/myPage";
     }
 }
